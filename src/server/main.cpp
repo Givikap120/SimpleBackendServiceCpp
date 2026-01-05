95ea0a18388f3b869219607ffda312ec7a0e515b
Givikap120 <givikap120@gmail.com>
2026-01-05 22:21:03 +0100

    Basic test of the functionality in main

---
+++ src/server/main.cpp
@@ -0,0 +1,28 @@
+﻿#include <iostream>
+#include "../core/executor.h"
+
+#include "../handlers/print_handler.h"
+
+#include "proto/jobs.grpc.pb.h"
+#include "proto/jobs.pb.h"
+
+int main() {
+	Executor executor;
+
+	// Register job handlers
+	executor.registerHandler<PrintHandler>();
+
+	executor.start(4);
+
+	// Push jobs
+	executor.addJob(Job(1, "print", "Hello world"));
+	executor.addJob(Job(2, "compute", "42"));
+
+	// Wait a bit for processing
+	std::this_thread::sleep_for(std::chrono::seconds(1));
+
+	// Shutdown executor
+	executor.shutdown();
+
+	return 0;
+}
