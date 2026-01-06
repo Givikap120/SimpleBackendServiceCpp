5c8107d48f86f18597c37146ef8d0d50238bcf3f
Givikap120 <givikap120@gmail.com>
2026-01-06 20:31:55 +0100

    move job result into a separate file

--- src/core/job_handler.h
+++ src/core/job_handler.h
@@ -1,42 +1,7 @@
 #pragma once
 
 #include "job.h"
-#include <string>
-
-struct JobResult
-{
-	bool success = false;
-	std::string result{};
-	std::string errorMessage{};
-
-	static JobResult Ok(std::string result = "")
-	{
-		return { true, std::move(result), "Job was completed successfully"};
-	}
-
-	static JobResult Fail(std::string message)
-	{
-		return { false, "FAIL", std::move(message)};
-	}
-
-	inline std::string log(bool printResult = true) const
-	{
-		std::ostringstream oss;
-
-		oss << "[Thread " << std::this_thread::get_id() << "] "
-			<< (success ? "Success" : "Failure");
-
-		if (printResult && success && !result.empty())
-			oss << ". " << result;
-
-		if (!success && !errorMessage.empty())
-			oss << ": " << errorMessage;
-
-		oss << "\n";
-
-		return oss.str();
-	}
-};
+#include "job_result.h"
 
 class JobHandler
 {
