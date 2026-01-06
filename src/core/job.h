36d66ae93edc6a250e7ae063330c6ae1c514db08
Givikap120 <givikap120@gmail.com>
2026-01-07 00:16:28 +0100

    Remove job timestamp

--- src/core/job.h
+++ src/core/job.h
@@ -11,18 +11,16 @@ public:
 	using Clock = std::chrono::system_clock;
 
 	Job(uint64_t id, std::string type, std::vector<std::byte> payload)
-		: m_id(id), m_type(std::move(type)), m_payload(std::move(payload)), m_createdAt(Clock::now())
+		: m_id(id), m_type(std::move(type)), m_payload(std::move(payload))
 	{
 	}
 
 	inline uint64_t id() const noexcept { return m_id; }
 	inline const std::string& type() const noexcept { return m_type; }
 	inline const std::vector<std::byte>& payload() const noexcept { return m_payload; }
-	inline Clock::time_point createdAt() const noexcept { return m_createdAt; }
 
 private:
 	uint64_t m_id = 0;
 	std::string m_type{};
 	std::vector<std::byte> m_payload{};
-	Clock::time_point m_createdAt = Clock::now();
 };
