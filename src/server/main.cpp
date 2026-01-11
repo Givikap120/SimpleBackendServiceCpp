09bac81c6fdfb629bcf12fe59726070b89d9fac9
Givikap120 <givikap120@gmail.com>
2026-01-11 18:46:14 +0100

    use rest server in main

--- src/server/main.cpp
+++ src/server/main.cpp
@@ -1,6 +1,7 @@
 ﻿#include <iostream>
 #include <nlohmann/json.hpp>
 
+#include "rest_server.h"
 #include "../core/executor.h"
 #include "../intermediate/job_submission_service.h"
 
@@ -12,17 +13,8 @@
 
 using json = nlohmann::json;
 
-int main() {
-	Executor executor;
-
-	// Register job handlers
-	executor.registerHandler<PrintHandler>();
-	executor.registerHandler<QuadraticEquationHandler>();
-
-	JobSubmissionService jobService(executor);
-
-	executor.start(4);
-
+void simpleTest(JobSubmissionService& jobService)
+{
 	// Push jobs
 	uint64_t idPrint = jobService.submit(ApiJobRequest("print", "Hello world"));
 
@@ -42,8 +34,29 @@ int main() {
 	std::optional<JobResult> resultEquation = jobService.getResult(idEquation);
 	std::cout << "Equation job (" << idEquation << ") status: " << to_string(jobService.getStatus(idEquation)) << "\n";
 	std::cout << "Equation job (" << idEquation << ") result: " << (resultEquation ? resultEquation.value().result : "NO RESULT") << "\n";
+}
 
-	// Shutdown executor
+int main() {
+	// Executor object runs the task via registered handlers
+	Executor executor;
+
+	// Register job handlers
+	executor.registerHandler<PrintHandler>();
+	executor.registerHandler<QuadraticEquationHandler>();
+
+	// Job submission service allows to submit jobs and query status/results
+	JobSubmissionService jobService(executor);
+
+	// Rest server handles HTTP requests and sends them to job service
+	RestServer server(jobService);
+
+	// Start executor with 4 threads
+	executor.start(4);
+
+	// Start REST server on port 8080
+	server.run(8080);
+
+	// Shutdown executor after server stops
 	executor.shutdown();
 
 	return 0;
