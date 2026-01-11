019a4f8308f542f093930f1c745cb09d8ba39c9e
Givikap120 <givikap120@gmail.com>
2026-01-11 21:18:04 +0100

    Rename the test category to ExecutorRunTest

--- tests/src/main/executor_run_test.cpp
+++ tests/src/main/executor_run_test.cpp
@@ -1,7 +1,10 @@
 #include <gtest/gtest.h>
 #include "test_utils.h"
 
-TEST_F(ExecutorTest, HandlesSuccessJob) {
+using ExecutorRunTest = ExecutorTest;
+
+TEST_F(ExecutorRunTest, HandlesSuccessJob)
+{
 	m_executor->start(1);
 	uint64_t okJobId = m_jobService->submit(ApiJobRequest("test", "Ok"));
 	auto result = waitForJob(*m_jobService, okJobId);
@@ -11,7 +14,8 @@ TEST_F(ExecutorTest, HandlesSuccessJob) {
 	EXPECT_EQ(result->errorMessage, "");
 }
 
-TEST_F(ExecutorTest, HandlesFailJob) {
+TEST_F(ExecutorRunTest, HandlesFailJob)
+{
 	m_executor->start(1);
 	uint64_t failJobId = m_jobService->submit(ApiJobRequest("test", "Fail"));
 	auto result = waitForJob(*m_jobService, failJobId);
@@ -21,7 +25,8 @@ TEST_F(ExecutorTest, HandlesFailJob) {
 	EXPECT_EQ(result->result, "");
 }
 
-TEST_F(ExecutorTest, HandlesExceptionJob) {
+TEST_F(ExecutorRunTest, HandlesExceptionJob)
+{
 	m_executor->start(1);
 	uint64_t exceptionJobId = m_jobService->submit(ApiJobRequest("test", "Unknown"));
 	auto result = waitForJob(*m_jobService, exceptionJobId);
