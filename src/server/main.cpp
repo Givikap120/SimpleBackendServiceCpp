a6a047147837770ff1ac4ddc145b1f187365ca7a
Givikap120 <givikap120@gmail.com>
2026-01-13 03:01:57 +0100

    Remove testing code

--- src/server/main.cpp
+++ src/server/main.cpp
@@ -1,5 +1,4 @@
 ﻿#include <iostream>
-#include <nlohmann/json.hpp>
 
 #include "rest_server.h"
 #include "core/executor.h"
@@ -7,34 +6,6 @@
 
 #include "handlers/print_handler.hpp"
 #include "handlers/quadratic_equation_handler.hpp"
-
-#include "proto/jobs.grpc.pb.h"
-#include "proto/jobs.pb.h"
-
-using json = nlohmann::json;
-
-void simpleTest(JobSubmissionService& jobService)
-{
-	// Push jobs
-	uint64_t idPrint = jobService.submit(ApiJobRequest("print", "Hello world"));
-
-	json payload;
-	payload["a"] = 1.0;
-	payload["b"] = -2.0;
-	payload["c"] = 1.0;
-	uint64_t idEquation = jobService.submit(ApiJobRequest("quadratic_equation", payload.dump()));
-
-	std::optional<JobResult> resultPrint = jobService.getResult(idPrint);
-	std::cout << "Print job (" << idPrint << ") status: " << to_string(jobService.getStatus(idPrint)) << "\n";
-	std::cout << "Print job (" << idPrint << ") result: " << (resultPrint ? resultPrint.value().result : "NO RESULT") << "\n";
-
-	// Wait a bit for processing
-	std::this_thread::sleep_for(std::chrono::seconds(1));
-
-	std::optional<JobResult> resultEquation = jobService.getResult(idEquation);
-	std::cout << "Equation job (" << idEquation << ") status: " << to_string(jobService.getStatus(idEquation)) << "\n";
-	std::cout << "Equation job (" << idEquation << ") result: " << (resultEquation ? resultEquation.value().result : "NO RESULT") << "\n";
-}
 
 int main() {
 	// Executor object runs the task via registered handlers
